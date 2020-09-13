b40770f6dbf555812dd3f7f51326c7b2a70767d1
Geraldina (laptop) <grushevskaya.d.v@yandex.ru>
2020-09-13 09:20:19 +0300

    Поправлено отображение точности таймера

--- example1/main.cpp
+++ example1/main.cpp
@@ -10,9 +10,9 @@ int main(int argc, char *argv[])
     std::system("chcp 65001"); // utf-8
     double start_time, end_time, tick;
     start_time = omp_get_wtime();
-    Sleep(5000);
+    Sleep(1000);
     end_time = omp_get_wtime();
     tick = omp_get_wtick();
-    printf("Время на замер времени %lf\n", end_time-start_time);
-    printf("Точность таймера %lf\n", tick);
+    printf("Время на замер времени %.16g\n", end_time-start_time);
+    printf("Точность таймера %.16g\n", tick);
 } 
