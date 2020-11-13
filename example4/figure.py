43d7bcd1d8dba2828803e51fe5f95e1e2c83f0f5
Geraldina (laptop) <grushevskaya.d.v@yandex.ru>
2020-11-13 23:52:42 +0300

    Графики

--- example4/figure.py
+++ example4/figure.py
@@ -3,24 +3,22 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator
 import numpy as np
 
-fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+N = 10
+x = np.arange(0, 50*N, 1)
 
-# Make data.
-X = np.arange(-5, 5, 0.25)
-Y = np.arange(-5, 5, 0.25)
-X, Y = np.meshgrid(X, Y)
-R = np.sqrt(X**2 + Y**2)
-Z = np.sin(R)
+fig = plt.figure(1)
+ax = fig.add_subplot(projection='3d')
 
-# Plot the surface.
-surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
-
-# Customize the z axis.
-ax.set_zlim(-1.01, 1.01)
-ax.zaxis.set_major_locator(LinearLocator(10))
-
-# Add a color bar which maps values to colors.
-fig.colorbar(surf, shrink=0.5, aspect=5)
-
+with open("output.txt", "r") as file:
+    while True:
+        data = file.readline()
+        if not data:
+            break
+        t = float(data)
+        if t > 0.005:
+            break
+        data = file.readline()
+        y = [float(i) for i in data.split()]
+        ax.scatter(xs=x, ys=t, zs=y)
+  
 plt.show()
