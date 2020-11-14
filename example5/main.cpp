d76d6ae5016537f1f510345ebeaacbb7d18aacd5
Geraldina (laptop) <grushevskaya.d.v@yandex.ru>
2020-11-14 21:03:22 +0300

    lec06_01

--- example5/main.cpp
+++ example5/main.cpp
@@ -1,35 +1,33 @@
 #pragma comment(lib, "C:\\Program Files (x86)\\Microsoft SDKs\\MPI\\Lib\\x86\\msmpi.lib")
 
-#include "mpi.h"
 #include <stdio.h>
+#include "mpi.h"
 int main(int argc, char **argv)
 {
-   int rank;
-   float a, b;
-   MPI_Status status;
-   MPI_Init(&argc, &argv);
+   int rank, size, prev, next;
+   int buf[2];
+   MPI_Request reqs[4];
+   MPI_Status stats[4];
+   MPI_Init(&argc,&argv);
+   MPI_Comm_size(MPI_COMM_WORLD, &size);
    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-   a = 0.0;
-   b = 0.0;
-   if(rank == 0)
-   {
-      b = 3.14159;
-	  MPI_Sendrecv(&b, 1, MPI_FLOAT, 1, 5, &a, 1, MPI_FLOAT, 1, 5, MPI_COMM_WORLD, &status);
-   }
-   if(rank == 1)
-   {
-      a = 2.71828;
-	  MPI_Sendrecv(&a, 1, MPI_FLOAT, 0, 5, &b, 1, MPI_FLOAT, 0, 5, MPI_COMM_WORLD, &status);
-   }
-   printf("process %d a = %f, b = %f\n", rank, a, b);
+   prev = rank - 1;
+   next = rank + 1;
+   if(rank==0) prev = size - 1;
+   if(rank==size - 1) next = 0;
+   MPI_Irecv(&buf[0], 1, MPI_INT, prev, 5, MPI_COMM_WORLD, &reqs[0]);
+   MPI_Irecv(&buf[1], 1, MPI_INT, next, 6, MPI_COMM_WORLD, &reqs[1]);
+   MPI_Isend(&rank, 1, MPI_INT, prev, 6, MPI_COMM_WORLD, &reqs[2]);
+   MPI_Isend(&rank, 1, MPI_INT, next, 5, MPI_COMM_WORLD, &reqs[3]);
+   MPI_Waitall(4, reqs, stats);
+   printf("process %d prev = %d next=%d\n", rank, buf[0], buf[1]);
    MPI_Finalize();
 }
 
 // запуск 
 // mpiexec -n 4 Debug\main.exe 
 
-
-// process 0 a = 2.718280, b = 3.141590
-// process 3 a = 0.000000, b = 0.000000
-// process 1 a = 2.718280, b = 3.141590
-// process 2 a = 0.000000, b = 0.000000
+// process 3 prev = 2 next=0
+// process 0 prev = 3 next=1
+// process 2 prev = 1 next=3
+// process 1 prev = 0 next=2
