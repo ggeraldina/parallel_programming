0c723c3dba96e44e427922ee8f8ac33e2697ad26
Geraldina (laptop) <grushevskaya.d.v@yandex.ru>
2020-11-19 12:43:49 +0300

    Заготовка

---
+++ example6/main.cpp
@@ -0,0 +1,108 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#define N 10
+#define NNN N*N*N
+
+void printfArray(double *d, int n, double t) {
+    printf("%.8f\n", t);
+    for (int i = 0; i < n; i++) {
+        printf("%.7f ", d[i]);
+    }
+    printf("\n");
+}
+
+void f(double t, double *x, double *dxdt, int n) {
+    int i = 0;
+    dxdt[i] = - 6 * x[i] * (x[i+1] - x[i-1+n]) * 0.5 * N
+              - (x[i+2] - 2 * x[i+1] + 2 * x[i-1+n] - x[i-2+n]) * 0.5 * NNN;
+    i = 1;
+    dxdt[i] = - 6 * x[i] * (x[i+1] - x[i-1]) * 0.5 * N
+              - (x[i+2] - 2 * x[i+1] + 2 * x[i-1] - x[i-2+n]) * 0.5 * NNN;
+    for (i = 2; i < n-2; i++)
+    {
+        dxdt[i] = - 6 * x[i] * (x[i+1] - x[i-1]) * 0.5 * N
+                  - (x[i+2] - 2 * x[i+1] + 2 * x[i-1] - x[i-2]) * 0.5 * NNN;
+    }
+    i = n-2;
+    dxdt[i] = - 6 * x[i] * (x[i+1] - x[i-1]) * 0.5 * N
+              - (x[i+2-n] - 2 * x[i+1] + 2 * x[i-1] - x[i-2]) * 0.5 * NNN;
+    i = n-1;
+    dxdt[i] = - 6 * x[i] * (x[i+1-n] - x[i-1]) * 0.5 * N
+              - (x[i+2-n] - 2 * x[i+1-n] + 2 * x[i-1] - x[i-2]) * 0.5 * NNN;
+}
+
+int rk4(int n, double t, double *x, double h, double finish) {
+
+    if (h <= 0 || (finish - t) <= 0) {
+        return -1;
+    }
+
+    double *k1, *k2, *k3, *k4;
+    double *temp;
+    k1 = (double*) malloc(n * sizeof (double));
+    k2 = (double*) malloc(n * sizeof (double));
+    k3 = (double*) malloc(n * sizeof (double));
+    k4 = (double*) malloc(n * sizeof (double));
+    temp = (double*) malloc(n * sizeof (double));
+
+    while (true) {
+        if (t > finish) {
+            break;
+        }               
+        
+        printfArray(x, n, t);
+
+        //k1
+        f(t, x, k1, n);
+        //k2
+        for (int i = 0; i < n; i++) {
+            temp[i] = x[i] + 0.5 * h * k1[i];
+        }
+        f(t + 0.5 * h, temp, k2, n);
+        //k3
+        for (int i = 0; i < n; i++) {
+            temp[i] = x[i] + 0.5 * h * k2[i];
+        }
+        f(t + 0.5 * h, temp, k3, n);
+        //k4
+        for (int i = 0; i < n; i++) {
+            temp[i] = x[i] + h * k3[i];
+        }
+        f(t + h, temp, k4, n);
+        //res
+        for (int i = 0; i < n; i++) {
+            x[i] += h * (k1[i] + 2 * k2[i] + 2 * k3[i] + k4[i]) / 6.0;
+        }
+
+        t += h; 
+    }
+
+    free(k1);
+    free(k2);
+    free(k3);
+    free(k4);
+    free(temp);
+    return 0;
+}
+
+int main(int argc, char * argv[]) {
+    system("chcp 65001"); // utf-8
+    freopen("output.txt", "w", stdout);
+    int n = 50*N; // amount x
+    double h = 0.001; // step t (time)
+    double *x = (double*) malloc(n * sizeof (double));
+    double from = 0.0, to = 10.0; // t (time)
+
+    double k = 1.0;
+    for (int i = 0; i < n; i++)
+    {
+        double xx = i*0.1;
+        x[i] = 2.0 * k * k / (cosh(k * (xx - 25)) * cosh(k * (xx - 25)));
+    }
+    rk4(n, from, x, h, to);
+    
+    free(x);
+    return 0;
+}
