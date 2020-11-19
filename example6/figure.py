27886339b2cdf7c4de2261e31fd242c6681836e3
Geraldina (laptop) <grushevskaya.d.v@yandex.ru>
2020-11-19 17:55:42 +0300

    2 графика

--- example6/figure.py
+++ example6/figure.py
@@ -7,7 +7,7 @@ N = 10
 x = np.arange(0, 50*N, 1)
 
 fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
+ax = fig.add_subplot(1, 2, 1, projection='3d')
 
 with open("output.txt", "r") as file:
     while True:
@@ -20,8 +20,24 @@ with open("output.txt", "r") as file:
             continue
         y = [float(i) for i in data.split()]
         ax.plot(xs=x, ys=[t]*len(x), zs=y, color="r", marker=".")
-        
-# ax.set_xlabel('x')
+
+ax = fig.add_subplot(1, 2, 2, projection='3d')
+ax.set_ylabel('t')
+ax.set_zlabel('u(x, t)')
+
+with open("output_2.txt", "r") as file:
+    while True:
+        data = file.readline()
+        if not data:
+            break
+        t = float(data)
+        data = file.readline()
+        if t * 10 % 10 != 0: 
+            continue
+        y = [float(i) for i in data.split()]
+        ax.plot(xs=x, ys=[t]*len(x), zs=y, color="r", marker=".")
+   
+
 ax.set_ylabel('t')
 ax.set_zlabel('u(x, t)')
   
